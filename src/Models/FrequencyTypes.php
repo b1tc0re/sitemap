b97b4d5dcc5a30ca92fed71612ae5739d04a754b
b1tc0re <b1tc0re@yandex.ru>
2021-02-06 13:45:30 +0300

    Типы частоты изменения страницы.

---
+++ src/Models/FrequencyTypes.php
@@ -0,0 +1,34 @@
+<?php namespace DeftCMS\Components\b1tc0re\Sitemap\Models;
+
+/**
+ * Типы частоты изменения страницы.
+ *
+ * @package     DeftCMS
+ * @author	    b1tc0re
+ * @copyright   2020-2021 DeftCMS (https://deftcms.ru/)
+ * @since	    Version 0.0.9a
+ */
+class FrequencyTypes
+{
+    const ALWAYS = 'always';
+    const HOURLY = 'hourly';
+    const DAILY = 'daily';
+    const WEEKLY = 'weekly';
+    const MONTHLY = 'monthly';
+    const YEARLY = 'yearly';
+    const NEVER = 'never';
+
+    /**
+     * Допустимые значения
+     * @var array
+     */
+    public static $validFrequency = [
+        self::ALWAYS,
+        self::HOURLY,
+        self::DAILY,
+        self::WEEKLY,
+        self::MONTHLY,
+        self::YEARLY,
+        self::NEVER
+    ];
+}
