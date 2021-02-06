4b2f87d38d2e2e698450bc6e509142ca59a4b642
b1tc0re <b1tc0re@yandex.ru>
2021-02-06 13:45:52 +0300

    Модель для каждой записи URL-адреса карты сайта

---
+++ src/Models/UrlModel.php
@@ -0,0 +1,159 @@
+<?php namespace DeftCMS\Components\b1tc0re\Sitemap\Models;
+
+/**
+ * Модель для каждой записи URL-адреса карты сайта
+ *
+ *
+ * @package     DeftCMS
+ * @author	    b1tc0re
+ * @copyright   2020-2021 DeftCMS (https://deftcms.ru/)
+ * @since	    Version 0.0.9a
+ */
+class UrlModel
+{
+    /**
+     * URL-адрес страницы
+     * @var string
+     */
+    protected $location;
+
+    /**
+     * Дата последнего изменения страницы.
+     *
+     * @var null|int
+     */
+    protected $lastModified = null;
+
+    /**
+     * Вероятная частота изменения этой страницы
+     *
+     * @var null|string
+     */
+    protected $changeFrequency = 'daily';
+
+    /**
+     * Priority (0.0-1.0). Default  0.5
+     * @var null|string
+     */
+    protected $priority = '0.5';
+
+    /**
+     * UrlModel constructor.
+     *
+     * @param array $params - Url parameters
+     */
+    public function __construct(array $params)
+    {
+        foreach ($params as $name => $value)
+        {
+            if( property_exists($this, $name) )
+            {
+                $setter = "set" . ucfirst($name);
+                $this->$setter($value);
+            }
+        }
+    }
+
+    /**
+     * Дата последнего изменения страницы.
+     *
+     * @param int|null $lastModified - timestamp
+     *
+     * @return $this
+     */
+    public function setLastModified($lastModified = null)
+    {
+        $this->lastModified = date('c');
+        $lastModified !== null && $this->lastModified = date('c', $lastModified);
+
+        return $this;
+    }
+
+    /**
+     * Устоновить URL-адрес страницы.
+     * @param string $location
+     *
+     * @throws \InvalidArgumentException
+     * @return $this
+     */
+    public function setLocation(string $location)
+    {
+        if( !filter_var($location, FILTER_VALIDATE_URL, FILTER_FLAG_PATH_REQUIRED) )
+        {
+            throw new \InvalidArgumentException("Please specify valid url.");
+        }
+
+        $this->location = $location;
+
+        return $this;
+    }
+
+    /**
+     * Устоновить приоритетность URL относительно других URL на Вашем сайте
+     *
+     * @param string|null $priority Default 0.5
+     *
+     * @return $this
+     */
+    public function setPriority(string $priority = null)
+    {
+        if ( $priority === null || !is_numeric($priority) || $priority < 0 || $priority > 1)
+        {
+            $priority = '0.5';
+        }
+
+        $this->priority = number_format($priority, 1, '.', ',');
+
+        return $this;
+    }
+
+    /**
+     * Устоновить частоту изменения этой страницы
+     * @param string|null $changeFrequency
+     *
+     * @return $this
+     */
+    public function setChangeFrequency(string $changeFrequency = null)
+    {
+        if( !in_array($changeFrequency, FrequencyTypes::$validFrequency, true) )
+        {
+            $changeFrequency = FrequencyTypes::DAILY;
+        }
+
+        $this->changeFrequency = $changeFrequency;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getChangeFrequency()
+    {
+        return $this->changeFrequency;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLastModified()
+    {
+        return $this->lastModified;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPriority()
+    {
+        return $this->priority;
+    }
+}
